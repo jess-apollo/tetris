50b91be90a72245fcd76b664b38a353b652e6cf1
JessKimSH <official1126@naver.com>
2015-08-28 16:45:05 +0900

    add a HTML_Parsing

---
+++ HTML_Parsing/app/src/main/java/com/example/user/html_parsing/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.user.html_parsing;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import com.example.user.html_parsing.net.Net_HTMLParse;
+
+public class MainActivity extends ActionBarActivity {
+
+    private ArrayList<HashMap<String, String>> data;
+    private Button bt_open;
+    private ListView list;
+    private SimpleAdapter sa;
+    private Net_HTMLParse hp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
